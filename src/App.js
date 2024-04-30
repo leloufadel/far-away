43ee72e0a0a35ce6c8df570f7ea475ab19114187
leloufadel <leloufadel@gmail.com>
2024-04-30 13:24:26 +0000

    building the layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import "./index.css";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      Hello ❤ 💕❤😍💖🎉🤳
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1> Far Away 💼 </h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3> what you need for your trip 😊? </h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em> 💼 You have X items on your List, and you alredy packed X (X%)</em>
+    </footer>
+  );
+}
+// export default App;
