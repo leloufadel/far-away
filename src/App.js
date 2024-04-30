8ae9330153359340cb159d17bba0cdee45cefb54
leloufadel <leloufadel@gmail.com>
2024-04-30 14:59:37 +0000

    rendering the item list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -12,8 +17,9 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1> Far Away 💼 </h1>;
+  return <h1> 🌴 Far Away 💼 </h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -22,7 +28,28 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+   </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style = { item.packed ?  {textDecoration: "line-through"} : {} }>
+        {" "}
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
